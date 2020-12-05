c39ab63ba697ed2935f6c886e6921bb0ca22bc4b
MikeDev96 <trikefx@live.co.uk>
2020-12-05 16:14:02 +0000

    + Added exercise 5

---
+++ ex5.js
@@ -0,0 +1,31 @@
+const fs = require("fs")
+
+const input = fs.readFileSync("ex5-input.txt", { encoding: "utf8" })
+
+const getMiddle = (str, lowerChar, upperChar, max) => {
+  let lower = 0
+  let upper = max - 1
+  
+  for (const l of str) {
+    if (l === lowerChar) {
+      upper -= Math.ceil((upper - lower) / 2)
+    }
+    else if (l === upperChar) {
+      lower += Math.ceil((upper - lower) / 2)
+    }
+  }
+
+  return lower
+}
+
+const getSeatId = boardingPass => {
+  const row = getMiddle(boardingPass.substr(0, 7), "F", "B", 128)
+  const column = getMiddle(boardingPass.substr(7, 3), "L", "R", 8)
+
+  return row * 8 + column
+}
+
+const boardingPasses = input.trim().split("\n")
+const highestSeatId = Math.max(...boardingPasses.map(bp => getSeatId(bp)))
+
+console.log(highestSeatId)
